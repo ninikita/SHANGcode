1f6f81752d7c0c4cf94b279c71f43f7950faf97a
Yaxin Yu <120191430+ninikita@users.noreply.github.com>
2026-04-10 22:46:13 -0700

    Update nn_exam_classification.py

--- nn_exam_classification.py
+++ nn_exam_classification.py
@@ -1,8 +1,8 @@
 ## Adapted from the AGNES codebase by Gupta et al. (MIT License).
 ## Source: https://github.com/kanangupta/AGNES
 
-## The current version of the file is set up to load and train on CIFAR-100.
-## The corresponding lines for CIFAR-10 and MNIST have been commented out.
+## The current version of the file is set up to load and train on CIFAR-10.
+## The corresponding lines for CIFAR-100 and MNIST have been commented out.
 ## This code can be used for other datasets by appropriately modifying the code defining
 ## train_dataset, train_transform, test_dataset, and test_transform.
 
@@ -10,7 +10,7 @@ import torch
 import torch.nn as nn
 import os, random
 from torchvision import datasets, models, transforms
-from SHNAG_optim import SHANG, ISHANG
+from nn_optim import *
 from util import *
 
 class Trainer:
@@ -59,15 +59,15 @@ class Trainer:
             transforms.RandomCrop(32, padding=4),
             transforms.RandomHorizontalFlip(),
             transforms.ToTensor(),
-            transforms.Normalize((0.5071, 0.4866, 0.4409), (0.2673, 0.2564, 0.2762)),
+            transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
         ])
 
 
         ### Only one of the following lines should be uncommented. Don't forget to use the same dataset
         ### for test_dataset as well.
-        train_dataset = datasets.CIFAR100('data/cifar', train=True, download=True, transform=train_transform)
+        #train_dataset = datasets.CIFAR100('data/cifar', train=True, download=True, transform=train_transform)
         #train_dataset = datasets.MNIST('data/mnist', train=True, download=False, transform=transforms.ToTensor())
-        #train_dataset = datasets.CIFAR10('data/cifar', train=True, download=True, transform=train_transform)
+        train_dataset = datasets.CIFAR10('data/cifar', train=True, download=True, transform=train_transform)
         data_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
         criterion = torch.nn.CrossEntropyLoss().to(self.device)
 
@@ -141,17 +141,17 @@ class Trainer:
         """
         self.net.eval()
 
-        ### For CIFAR-10 change the last transform to transforms.Normalize((runs23.4914, runs23.4822, runs23.4465), (runs23.2023, runs23.1994, runs23.2010))
-        ### For CIFAR-100 change the last transform to transforms.Normalize((runs23.5071, runs23.4866, runs23.4409), (runs23.2673, runs23.2564, runs23.2762))
+        ### For CIFAR-10 change the last transform to transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
+        ### For CIFAR-100 change the last transform to transforms.Normalize((0.5071, 0.4866, 0.4409), (0.2673, 0.2564, 0.2762))
         ### For MNIST, remove all transforms except transforms.ToTensor()
         test_transform = transforms.Compose([transforms.ToTensor(),
-                                             transforms.Normalize((0.5071, 0.4866, 0.4409), (0.2673, 0.2564, 0.2762)),
+                                             transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
                                              ])
 
         ### Only one of the following lines should be uncommented. Ideally, the same dataset that's used for training.
-        test_dataset = datasets.CIFAR100('data/cifar', train=False, download=True, transform=test_transform)
+        #test_dataset = datasets.CIFAR100('data/cifar', train=False, download=True, transform=test_transform)
         #test_dataset = datasets.MNIST('data/mnist', train=False, download=True, transform=transforms.ToTensor())
-        #test_dataset = datasets.CIFAR10('data/cifar', train=False, download=False, transform=test_transform)
+        test_dataset = datasets.CIFAR10('data/cifar', train=False, download=False, transform=test_transform)
 
         data_loader = torch.utils.data.DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
         criterion = torch.nn.CrossEntropyLoss().to(self.device)
@@ -231,8 +231,8 @@ for run, seed in enumerate(seeds):
     torch.manual_seed(seed)
 
     opt_names = {
-        'SHANG,alpha=.5,gamma=15': 'SHANG(self.net.parameters(), alpha={} , time_scale={}, weight_decay={})'.format(0.5, 15, 1e-5),
-        'ISHANG,alpha=.5,gamma=15': 'ISHANG(self.net.parameters(), alpha={} , time_scale={}, rho = {}, weight_decay={})'.format(0.5, 15, 1.5, 1e-5),
+        'SHANG': 'SHANG(self.net.parameters(), alpha={} , time_scale={}, weight_decay={})'.format(0.5, 10, 1e-5),
+        'SHANG++': 'SHANGPlus(self.net.parameters(), alpha={} , time_scale={}, rho = {}, weight_decay={})'.format(0.5, 10, 1.5, 1e-5),
         'AGNES': 'AGNES(self.net.parameters(), lr={} , momentum={} , correction={}, weight_decay = {})'.format(0.01, 0.99,  0.001, 1e-5),
         'NAG': 'AGNES(self.net.parameters(), lr={} , momentum={} , correction={}, weight_decay={})'.format(1e-3, 0.99, 1e-3, 1e-5),
         'ADAM': 'torch.optim.Adam(self.net.parameters(), lr=1e-3, weight_decay=1e-5)',
@@ -258,16 +258,16 @@ for run, seed in enumerate(seeds):
         #    nn.Linear(84, 10)
         #)
 
-        model = models.resnet50(pretrained=True)
-        model.fc = nn.Linear(model.fc.in_features, 100, bias=True)
+       # model = models.resnet50(pretrained=True)
+        #model.fc = nn.Linear(model.fc.in_features, 100, bias=True)
 
-        #model = models.resnet34(pretrained=True)
-        #model.fc = nn.Linear(model.fc.in_features, 10, bias=True)
+        model = models.resnet34(pretrained=True)
+        model.fc = nn.Linear(model.fc.in_features, 10, bias=True)
         device = torch.device("mps" if torch.backends.mps.is_available() else "cpu")
         model = model.to(device)
         net = Trainer(model=model, opt_name=opt_name)
 
-        net.train(save_dir = 'CIFAR100-ResNet50_batch50_for5runs'+key+'/'+str(run),
+        net.train(save_dir = 'CIFAR10-ResNet34_batch50_for5runs'+key+'/'+str(run),
                   batch_size=50,
                   num_epochs = 50,
                   schedule_lr_epochs=25,
@@ -276,3 +276,129 @@ for run, seed in enumerate(seeds):
                   manual_seed=False,
                   verbose=False)
 
+
+import torch
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+data = {}
+runs = 5
+# For MNIST, train_size = 60000
+# For CIFAR10/100, train_size = 50000
+train_size = 50000
+batch = 50
+epochs = 50
+epoch_step = train_size / batch
+total_steps = epoch_step * epochs
+title = "CIFAR10-ResNet34_batch50_for5runs"
+
+names = [
+    'SGD',
+    'SHB',
+    'NAG',
+    'ADAM',
+    'SNAG',
+    'AGNES',
+    'SHANG',
+    'SHANG++',
+]
+
+# 设置颜色和线型
+colors = {}
+linestyles = {}
+style_list = ['-', '--', ':']
+
+color_map = {
+    'SHANG++': 'red',
+    'SHANG': 'green',
+    'AGNES': 'blue',
+    'SNAG': 'orange',
+    'SGD': 'gray',
+    'SHB': 'black',
+    'NAG': 'olive',
+    'ADAM': 'yellow'
+}
+
+class_count = {key: 0 for key in color_map}
+
+for name in names:
+    for key in color_map:
+        if name.startswith(key):
+            colors[name] = color_map[key]
+            linestyles[name] = style_list[class_count[key] % len(style_list)]
+            class_count[key] += 1
+            break
+    else:
+        colors[name] = 'black'
+        linestyles[name] = '-'
+
+metrics = ['Test Accuracy', 'Test Loss', 'Training Loss']
+decay = 0.999
+
+for name in names:
+    data[name] = {'Test Loss': [], 'Training Loss': [], 'Test Accuracy': []}
+    for i in range(runs):
+        with open(title + name + '/' + str(i) + '/checkpoint_50.pth', 'rb') as file:
+            temp = torch.load(file, map_location=torch.device('cpu'))
+            data[name]['Test Loss'].append(temp['test_losses'])
+            data[name]['Test Accuracy'].append(temp['test_accuracies'])
+            running_averages = []
+            last = temp['train_losses'][0]
+            for num in temp['train_losses']:
+                last = decay * last + (1 - decay) * num
+                running_averages.append(last)
+            data[name]['Training Loss'].append(running_averages)
+
+for name in names:
+    data[name]['Test Accuracy'] = 100 * np.array(data[name]['Test Accuracy'])
+    for metric in metrics[1:]:
+        data[name][metric] = np.array(data[name][metric])
+
+# --- Test Accuracy ---
+metric = metrics[0]
+plt.figure()
+for name in names:
+    mean = np.mean(data[name][metric], axis=0)
+    std = np.std(data[name][metric], axis=0)
+    x_vals = np.arange(0, total_steps + 1, epoch_step)
+    plt.plot(x_vals, mean, label=name, color=colors[name], linestyle=linestyles[name])
+    plt.fill_between(x_vals, mean + std, mean - std, alpha=0.2, color=colors[name])
+plt.ylim([95.5, 99.5])
+plt.xlabel("Training steps")
+plt.ylabel("Test Accuracy (%)")
+#plt.title(title + metric)
+plt.legend()
+plt.savefig(title + metric)
+
+# --- Test Loss ---
+metric = metrics[1]
+plt.figure()
+for name in names:
+    mean = np.clip(np.mean(data[name][metric], axis=0), 1e-8, None)
+    std = np.std(data[name][metric], axis=0)
+    x_vals = np.arange(0, total_steps + 1, epoch_step)
+    plt.semilogy(x_vals, mean, label=name, color=colors[name], linestyle=linestyles[name])
+    plt.fill_between(x_vals, mean + std, mean - std, alpha=0.2, color=colors[name])
+plt.title(title + metric)
+plt.xlabel("Training steps")
+plt.ylabel("Loss (log scale)")
+plt.legend()
+plt.savefig(title + metric)
+plt.show()
+
+# --- Training Loss ---
+metric = metrics[2]
+plt.figure()
+for name in names:
+    mean = np.mean(data[name][metric], axis=0)
+    std = np.std(data[name][metric], axis=0)
+    x_vals = range(len(mean))
+    plt.semilogy(x_vals, mean, label=name, color=colors[name], linestyle=linestyles[name])
+    plt.fill_between(x_vals, mean + std, mean - std, alpha=0.2, color=colors[name])
+plt.title(title + metric)
+plt.xlabel("Training steps")
+plt.ylabel("Loss (log scale)")
+plt.legend()
+plt.savefig(title + metric)
+plt.show()
